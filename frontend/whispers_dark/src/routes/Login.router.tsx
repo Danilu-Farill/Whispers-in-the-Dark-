936bd8d1fd8ac8e6339fdaf0c33edc63576a64c7
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-10-25 18:38:52 -0600

    connection change

--- frontend/whispers_dark/src/routes/Login.router.tsx
+++ frontend/whispers_dark/src/routes/Login.router.tsx
@@ -7,7 +7,7 @@ export const useLoginConnection = () => {
   const navigate = useNavigate();
   const loginUser = async ({email, password}: IUser) => {
     try {
-      const response = await fetch(`http:whispers-in-the-dark-phi.vercel.app/home/users/create`, {
+      const response = await fetch(`https://whispers-in-the-dark-phi.vercel.app/home/users/create`, {
         method: "POST",
         mode: 'no-cors',
         headers: {
