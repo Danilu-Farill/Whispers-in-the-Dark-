b0b6aac452de39c5d12866dd67fc16a1d03b9c5d
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-11-19 18:39:16 -0600

    feat: changes index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,11 +9,15 @@ import cors from 'cors';
 
 dotenv.config();
 const app: Application = express();
-const port:number = parseInt(process.env.PORT || "4001");
+const port = process.env.PORT || 3000;
+const frontendUrl = process.env.FRONTEND_URL;
+if (!frontendUrl) {
+  throw new Error('FRONTEND_URL no está definido');
+}
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: ['https://whispers-in-the-dark-phi.vercel.app', 'http://localhost:5173'],
+  origin: [frontendUrl, 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
